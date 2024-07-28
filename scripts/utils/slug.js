076d5d9a33841e3a47aa461fa3500b15c6553b60
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-07-29 02:27:22 +0900

    Set up ESLint using `eslint-config-stylelint`

--- scripts/utils/slug.js
+++ scripts/utils/slug.js
@@ -4,14 +4,12 @@
 // - make sure the slug is a safe file name
 // - add a string sortable prefix to preserve the original order
 export default function generateSlug(name, index) {
-	const prefix = (`000` + index.toString()).slice(-3);
+	const prefix = `000${index}`.slice(-3);
 
-	return (
-		prefix +
-		'-' +
-		name
-			.toLowerCase()
-			.replace(/^@/g, '') // Remove the scope character.
-			.replace(/[^a-z0-9-_]/g, '-')
-	);
+	const convertedName = name
+		.toLowerCase()
+		.replace(/^@/g, '') // Remove the scope character.
+		.replace(/[^a-z0-9-_]/g, '-');
+
+	return `${prefix}-${convertedName}`;
 }
