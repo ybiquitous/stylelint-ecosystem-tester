bd0a65b7bf7a4e83ac902be9e825f61a68fa7292
Romain Menke <romainmenke@gmail.com>
2024-07-28 13:37:04 +0200

    wip

--- scripts/update-workflows.js
+++ scripts/update-workflows.js
@@ -1,36 +1,36 @@
 import { readdirSync, rmSync, readFileSync, writeFileSync } from 'node:fs';
 import { fileURLToPath } from 'node:url';
 import { parse, stringify } from 'yaml';
-import generateSlug from './slug.mjs';
+import generateSlug from './utils/slug.js';
 
 // Clean existing workflows for packages that no longer exist.
-const workflowsDir = new URL('../../.github/workflows', import.meta.url);
+const workflowsDir = new URL('../.github/workflows', import.meta.url);
 
 readdirSync(workflowsDir).forEach((file) => {
 	if (!file.startsWith('test-package-')) return;
 
-	const workflowFile = new URL(`../../.github/workflows/${file}`, import.meta.url);
+	const workflowFile = new URL(`../.github/workflows/${file}`, import.meta.url);
 	rmSync(fileURLToPath(workflowFile), { force: true });
 });
 
 // List all packages
-const ecosystemDataFile = new URL('../../data/ecosystem.yml', import.meta.url);
+const ecosystemDataFile = new URL('../data/ecosystem.yml', import.meta.url);
 const ecosystemDataContent = readFileSync(ecosystemDataFile, 'utf8');
 const ecosystemData = parse(ecosystemDataContent);
 
 // Read the workflow template
-const workflowTemplateFile = new URL('../../templates/test-package.yml', import.meta.url);
+const workflowTemplateFile = new URL('../templates/test-package.yml', import.meta.url);
 const workflowTemplateContent = readFileSync(workflowTemplateFile, 'utf8');
 
 // Generate workflows for each package
-ecosystemData.packages.forEach((name) => {
+ecosystemData.packages.forEach((pkg, index) => {
 	const workflow = parse(workflowTemplateContent);
 
-	const slug = generateSlug(name);
+	const slug = generateSlug(pkg, index);
 	const workflowFilePath = `.github/workflows/test-package-${slug}.yml`;
 
 	// Set the name of the workflow.
-	workflow.name = `Test ${name}`;
+	workflow.name = `Test ${pkg}`;
 
 	// Run the workflow on changes to the workflow file itself.
 	workflow.on.push.paths.push(workflowFilePath);
@@ -41,9 +41,9 @@ ecosystemData.packages.forEach((name) => {
 
 	// Set the inputs
 	workflow.jobs.test.with ??= {};
-	workflow.jobs.test.with.package = name;
+	workflow.jobs.test.with.package = pkg;
 
-	const workflowFile = new URL(`../../${workflowFilePath}`, import.meta.url);
+	const workflowFile = new URL(`../${workflowFilePath}`, import.meta.url);
 	const workflowContent = stringify(workflow);
 
 	writeFileSync(fileURLToPath(workflowFile), workflowContent, 'utf8');
