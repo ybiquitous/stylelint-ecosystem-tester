6198e359216a5d9d73e38442de3b16f679e4b8d3
Romain Menke <romainmenke@gmail.com>
2024-07-28 14:23:25 +0200

    split latest and next

--- scripts/update-workflows.js
+++ scripts/update-workflows.js
@@ -3,11 +3,39 @@ import { fileURLToPath } from 'node:url';
 import { parse, stringify } from 'yaml';
 import generateSlug from './utils/slug.js';
 
+function generateWorkflow({
+	concurrencyGroup,
+	pkg,
+	stylelintVersion,
+	template,
+	workflowFilePath,
+	workflowName,
+}) {
+	const workflow = parse(template);
+
+	// Set the workflow name
+	workflow.name = workflowName;
+
+	// Run the workflow on changes to the workflow file itself.
+	workflow.on.push.paths.push(workflowFilePath);
+	workflow.on.pull_request.paths.push(workflowFilePath);
+
+	// Set concurrency
+	workflow.concurrency.group = concurrencyGroup;
+
+	// Set the package to test and the Stylelint version to use.
+	workflow.jobs.test.with ??= {};
+	workflow.jobs.test.with.package = pkg;
+	workflow.jobs.test.with['stylelint-version'] = stylelintVersion;
+
+	return workflow;
+}
+
 // Clean existing workflows for packages that no longer exist.
 const workflowsDir = new URL('../.github/workflows', import.meta.url);
 
 readdirSync(workflowsDir).forEach((file) => {
-	if (!file.startsWith('test-package-')) return;
+	if (!file.startsWith('test-package.')) return;
 
 	const workflowFile = new URL(`../.github/workflows/${file}`, import.meta.url);
 	rmSync(fileURLToPath(workflowFile), { force: true });
@@ -24,27 +52,35 @@ const workflowTemplateContent = readFileSync(workflowTemplateFile, 'utf8');
 
 // Generate workflows for each package
 ecosystemData.packages.forEach((pkg, index) => {
-	const workflow = parse(workflowTemplateContent);
-
 	const slug = generateSlug(pkg, index);
-	const workflowFilePath = `.github/workflows/test-package-${slug}.yml`;
 
-	// Set the name of the workflow.
-	workflow.name = pkg;
+	const latestStylelintWorkflowFilePath = `.github/workflows/test-package.${slug}.latest.yml`;
+	const latestStylelintWorkflow = generateWorkflow({
+		concurrencyGroup: '${{ github.workflow }}-${{ github.ref }}-' + slug + '-latest',
+		pkg,
+		stylelintVersion: 'stylelint@latest',
+		template: workflowTemplateContent,
+		workflowFilePath: latestStylelintWorkflowFilePath,
+		workflowName: 'latest',
+	});
 
-	// Run the workflow on changes to the workflow file itself.
-	workflow.on.push.paths.push(workflowFilePath);
-	workflow.on.pull_request.paths.push(workflowFilePath);
+	const latestWorkflowFile = new URL(`../${latestStylelintWorkflowFilePath}`, import.meta.url);
+	const latestWorkflowContent = stringify(latestStylelintWorkflow);
 
-	// Set concurrency
-	workflow.concurrency.group = '${{ github.workflow }}-${{ github.ref }}-' + slug;
+	writeFileSync(fileURLToPath(latestWorkflowFile), latestWorkflowContent, 'utf8');
 
-	// Set the inputs
-	workflow.jobs.test.with ??= {};
-	workflow.jobs.test.with.package = pkg;
+	const nextStylelintWorkflowFilePath = `.github/workflows/test-package.${slug}.next.yml`;
+	const nextStylelintWorkflow = generateWorkflow({
+		concurrencyGroup: '${{ github.workflow }}-${{ github.ref }}-' + slug + '-next',
+		pkg,
+		stylelintVersion: 'stylelint/stylelint',
+		template: workflowTemplateContent,
+		workflowFilePath: nextStylelintWorkflowFilePath,
+		workflowName: 'next',
+	});
 
-	const workflowFile = new URL(`../${workflowFilePath}`, import.meta.url);
-	const workflowContent = stringify(workflow);
+	const nextWorkflowFile = new URL(`../${nextStylelintWorkflowFilePath}`, import.meta.url);
+	const nextWorkflowContent = stringify(nextStylelintWorkflow);
 
-	writeFileSync(fileURLToPath(workflowFile), workflowContent, 'utf8');
+	writeFileSync(fileURLToPath(nextWorkflowFile), nextWorkflowContent, 'utf8');
 });
